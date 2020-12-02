323ffe0db33f836400c77085a072c249efaffac3
Hugo Franco <hugofranco_12@hotmail.com>
2020-12-01 23:51:02 -0300

    createTask adicionado.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { get } from 'http';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
-    //A injeção de dependência é feita assim no typescript, ela é feita pelo construtor.
-    constructor(private tasksService: TasksService) {}
-    @Get()
-    getAllTasks(): Task [] {
-        return this.tasksService.getAllTasks();
-    }
+  //A injeção de dependência é feita assim no typescript, ela é feita pelo construtor.
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
-     
-
