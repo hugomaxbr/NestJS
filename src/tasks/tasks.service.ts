2860a0037230eec460e38c5ff0a76903039619e6
Hugo Franco <hugofranco_12@hotmail.com>
2020-12-04 18:55:44 -0300

    editTaskById service and editStatusById service added.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { from } from 'rxjs';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { CreatTaskDto } from './dto/createTask.dto';
 import { EditTaskDto } from './dto/editTask.dto';
 
@@ -12,8 +12,8 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(createTaskDto : CreatTaskDto): Task {
-    const {title, description} = createTaskDto;
+  createTask(createTaskDto: CreatTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
@@ -25,19 +25,31 @@ export class TasksService {
     return task;
   }
 
-  getTaskById(id: string) : Task {
-    return this.tasks.find(task => task.id === id);
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
   }
 
   deleteTaskById(id: string) {
     const found = this.getTaskById(id);
-    const foundIndex = this.tasks.findIndex(task => task.id === found.id)
+    const foundIndex = this.tasks.findIndex((task) => task.id === found.id);
     return this.tasks.splice(foundIndex, 1);
   }
 
-  editTaskById(id : string, editTaskDto : EditTaskDto) {
-    const oldTask = this.getTaskById(id);
-    return this.tasks.forEach(task => task.id === oldTask.id ? { ...oldTask, task   } : task );
+  editStatusById(id: string, status: TaskStatus): Task {
+    const found = this.getTaskById(id);
+    found.status = status;
+    return found;
   }
 
+  editTaskById(id: string, editTaskDto: EditTaskDto): Task {
+    const { title, description } = editTaskDto;
+    const oldTask = this.getTaskById(id);
+    if (title) {
+      oldTask.title = title;
+    }
+    if (description) {
+      oldTask.description = description;
+    }
+    return oldTask;
+  }
 }
