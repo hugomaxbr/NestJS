0eb0eba75ec2311e196fa16b2cdd6d3e8d4b3064
Hugo Franco <hugofranco_12@hotmail.com>
2020-12-04 18:54:58 -0300

    feature: editTaskById controller and editStatusById controller added.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { get } from 'http';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { title } from 'process';
 import { CreatTaskDto } from './dto/createTask.dto';
 import { EditTaskDto } from './dto/editTask.dto';
@@ -15,22 +24,30 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
   @Post()
-  createTask(@Body() createTaskDto : CreatTaskDto): Task {
+  createTask(@Body() createTaskDto: CreatTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  @Get("/:id")
-  getTaskById(@Param("id") id: string ) : Task {
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskById(id);
   }
 
   @Delete()
-  deleteTaskById(@Param("id") id: string){
-    return this.tasksService.deleteTaskById(id)
-  }
-  @Patch("/:id")
-  editTaskById(@Param("id") id : string, @Body() editTaskDto : EditTaskDto){
-    return this.tasksService.editTaskById(id, editTaskDto);
+  deleteTaskById(@Param('id') id: string) {
+    return this.tasksService.deleteTaskById(id);
   }
 
+  @Patch('/:id/status')
+  editStatusById(
+    @Body('taskStatus') taskStatus: TaskStatus,
+    @Param('id') id: string,
+  ) {
+    return this.tasksService.editStatusById(id, taskStatus);
+  }
+
+  @Put('/:id')
+  editTaskById(@Param('id') id: string, @Body() editTaskDto: EditTaskDto) {
+    return this.tasksService.editTaskById(id, editTaskDto);
+  }
 }
