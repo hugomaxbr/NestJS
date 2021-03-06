d6fd93d92b1a2770b82420fa9df0a6ab321ba0c4
Hugo Franco Alves Gil <hugofranco_12@hotmaiil.com>
2021-03-06 17:06:45 -0300

    fix: prettier autosave problem fixed, ignore is no longer useful

--- src/tasks/config/typeorm.config.ts
+++ src/tasks/config/typeorm.config.ts
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'taskmanagement',
-  autoLoadEntities: true,
-  synchronize: true,
-}
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'taskmanagement',
+    autoLoadEntities: true,
+    synchronize: true,
+};
