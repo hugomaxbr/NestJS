fc7e620affac3940770deb25c1b2070f98f4c3d5
Hugo Franco Alves Gil <hugofranco_12@hotmaiil.com>
2021-03-08 17:54:17 -0300

    uptade: createTask now can only delete tasks owned by the user

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,13 +3,16 @@ import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/createTask.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/getTasksFilter.dto';
+import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTasks(filterDTO: GetTasksFilterDto): Promise<Task[]> {
+    async getTasks(filterDTO: GetTasksFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDTO;
 
         const query = this.createQueryBuilder('task');
+
+        query.where('task.userId = :userId', { userId: user.id });
 
         if (status) {
             query.andWhere('task.status = :status', { status });
@@ -25,14 +28,16 @@ export class TaskRepository extends Repository<Task> {
         return tasks;
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const { title, description } = createTaskDto;
         const task = new Task();
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
+        task.user = user;
         await task.save();
-
+        delete task.user;
+        delete task.userId;
         return task;
     }
 }
