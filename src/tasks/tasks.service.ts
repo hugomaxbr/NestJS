1e646e0dd48b7caf44494d03ab581b4973a4d7c4
Hugo Franco Alves Gil <hugofranco_12@hotmaiil.com>
2021-03-08 17:58:00 -0300

    update:  users can only access, add, modify or delete  their given tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
 import { CreateTaskDto } from './dto/createTask.dto';
 import { GetTasksFilterDto } from './dto/getTasksFilter.dto';
 import { TaskStatus } from './task-status.enum';
@@ -13,16 +14,18 @@ export class TasksService {
         private taskRepository: TaskRepository
     ) {}
 
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
-        return this.taskRepository.getTasks(filterDto);
+    async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto, user);
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.taskRepository.createTask(createTaskDto);
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        return this.taskRepository.createTask(createTaskDto, user);
     }
 
-    async getTaskById(id: number): Promise<Task> {
-        const found = await this.taskRepository.findOne(id);
+    async getTaskById(id: number, user: User): Promise<Task> {
+        const found = await this.taskRepository.findOne({
+            where: { id, userId: user.id },
+        });
 
         if (!found) {
             throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -30,15 +33,22 @@ export class TasksService {
         return found;
     }
 
-    async deleteTaskById(id: number): Promise<void> {
-        const deleted = await this.taskRepository.delete(id);
+    async deleteTaskById(id: number, user: User): Promise<void> {
+        const deleted = await this.taskRepository.delete({
+            id,
+            userId: user.id,
+        });
         if (deleted.affected === 0) {
             throw new NotFoundException(`Task with ID "${id}" not found`);
         }
     }
 
-    async editStatusById(id: number, status: TaskStatus): Promise<Task> {
-        const task = await this.getTaskById(id);
+    async editStatusById(
+        id: number,
+        status: TaskStatus,
+        user: User
+    ): Promise<Task> {
+        const task = await this.getTaskById(id, user);
         task.status = status;
         await task.save();
         return task;
